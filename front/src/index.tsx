1dd8a39242229599719a964bca3dcc4ebf5f79f1
Romain Jacquiez <r.jacquiez@la-boite-immo.com>
2020-05-02 06:00:59 +0200

    [++][ADD][Index] Setup index of front with scss

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+import './styles/_reset.scss';
+
 import App from './components/App';
 
 ReactDOM.render(
